f722fbdc421a0312e887fdd25b3c6dcb0d08edfa
Oranore <60654293+Oranore@users.noreply.github.com>
2020-04-20 12:29:24 +0200

    nice simpsons quotes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import DisplayQuote from './components/DisplayQuote';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: this.getQuote
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
 
+  getQuote() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          quote: data[0],
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayQuote quote={this.state.quote} /> 
+        <button type="button" onClick={this.getQuote}>Change quote</button>
+      </div>
+    );
+  }
+}
 export default App;
